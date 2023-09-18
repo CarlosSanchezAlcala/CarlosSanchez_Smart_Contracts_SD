1d47e1eeabc2d761adacb51ecbb89d9da6af98f8
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2023-09-18 17:04:25 -0500

    File Smart Contract - SD

--- contracts/Wallet_PIB_Definition.sol
+++ contracts/Wallet_PIB_Definition.sol
@@ -19,6 +19,12 @@ contract Wallet_PIB_Definition {
         return balances[account];
     }
 
+    /// @dev Enviar deposito, si no cumple el valor especificado, te da un mensaje de error
+    function sendDeposit(address addr, uint256 amount) public payable {
+        require(msg.value == amount, "El valor enviado o depositado, no coincide con la cantidad especificada.");
+        balances[addr] += amount;
+    }
+
     receive() external payable {
 
     }
