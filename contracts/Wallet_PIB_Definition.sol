d8a480b57b64019c276874b5e9796d0aeed14f5a
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2023-09-18 11:25:21 -0500

    File Smart Contract - SD

--- contracts/Wallet_PIB_Definition.sol
+++ contracts/Wallet_PIB_Definition.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+/**
+* @title Contrato inteligente de una Wallet (Envió y recibimiento de dinero).
+*/
+
 contract Wallet_PIB_Definition {
     constructor(){
 
